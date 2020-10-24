bc23196d8f6b82e532201cb77d365c3a11e48b5a
octavian.bodnariu <octavian.bodnariu@bearingpoint.com>
2020-10-24 21:02:37 +0300

    Connect Four: initial commit

---
+++ connect_four/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.width = 1024
+    t.window.height = 768
+    t.version = "11.3"                  -- The LÖVE version this game was made for (string)
+    t.window.title = "Connect Four"         -- The window title (string)
+
+end
